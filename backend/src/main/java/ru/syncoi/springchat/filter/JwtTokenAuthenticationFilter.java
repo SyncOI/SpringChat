349e030717ec4b902532b40a6d29bc694ec56c2b
SyncOI <3ZdQim06>
2022-03-29 18:04:45 +0300

    Finished JWT filter

--- backend/src/main/java/ru/syncoi/springchat/filter/JwtTokenAuthenticationFilter.java
+++ backend/src/main/java/ru/syncoi/springchat/filter/JwtTokenAuthenticationFilter.java
@@ -3,7 +3,11 @@ package ru.syncoi.springchat.filter;
 import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+import ru.syncoi.springchat.config.JwtConfig;
 import ru.syncoi.springchat.service.JwtTokenProvider;
 import ru.syncoi.springchat.service.UserService;
 
@@ -16,31 +20,38 @@ import java.io.IOException;
 public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
     private JwtTokenProvider tokenProvider;
+    private JwtConfig jwtConfig;
     private UserService userService;
 
-    public JwtTokenAuthenticationFilter(JwtTokenProvider tokenProvider, UserService userService) {
+    public JwtTokenAuthenticationFilter(JwtTokenProvider tokenProvider, JwtConfig jwtConfig, UserService userService) {
         this.tokenProvider = tokenProvider;
+        this.jwtConfig = jwtConfig;
         this.userService = userService;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        String header = request.getHeader("Authorization");
+        String header = request.getHeader(jwtConfig.getHeader());
 
-        if (header == null || !header.startsWith("Bearer ")) {
+        if (header == null || !header.startsWith(jwtConfig.getPrefix())) {
             chain.doFilter(request, response);
             return;
         }
 
-        String token = header.replace("Bearer ", "");
+        String token = header.replace(jwtConfig.getPrefix(), "");
 
         if (tokenProvider.validateToken(token)) {
             Claims claims = tokenProvider.getClaimsFromJWT(token);
             String username = claims.getSubject();
 
-//            UsernamePasswordAuthenticationToken auth =
-//                    userService.loadUserByUsername()
+            UserDetails userDetails =
+                    userService.loadUserByUsername(username);
 
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         } else {
             SecurityContextHolder.clearContext();
         }
